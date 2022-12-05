3a5228bc0b0e31acd6d7cf35b305d9535f0fd7fd
Sumaiya <44579268+SumaiyaKA@users.noreply.github.com>
2022-12-05 15:36:44 +0400

    Basic Level Development (#2)
    * create api call to get data
    
    * Cleanup
    
    Co-authored-by: Sumaiya <d17126680@mydit.ie>

---
+++ code/nav.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>Harbour Space</title>
+</head>
+<body>
+  <!-- navigation -->
+  <header>
+    <nav>
+    <div class="menu">
+    <button class="menu-btn">Dropdown</button>
+    <div class="menu-content">
+        <a href="index.php">Home</a>
+    </div>
+    </div>
+      <!-- <ul>
+        <li><a href="index.php">HarbourSpace</a></li> -->
+        <?php
+            // don't show any error
+            error_reporting(0);
+
+        ?>
+      <!-- </ul> -->
+    </nav>
+  </header>
+</body>
+</html>
